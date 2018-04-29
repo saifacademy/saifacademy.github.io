62e81544830e734e5a36531b8ea49432de61ab5f
Saif Bashar <asylum2elysium@gmail.com>
2018-04-29 22:34:27 +0300

    add

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$servername = "localhost";
-$database="std";
-$username = "root";
-$password = "";
-$conn = new mysqli($servername, $username, $password, $database);
-$charset= 'SET CHARACTER SET utf8'; 
-mysqli_query($conn,$charset);
-?>
--- footer.php
+++
@@ -1,4 +0,0 @@
-<div class="page-footer center white-text text-darken-2 grey darken-3" style="margin:0px">
-        &copy; Saif Bashar 2018
-</div>
-        
