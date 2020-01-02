84864e76176553303a5cfaf27e2e353f555a111d
Saif Bashar <asylum2elysium@gmail.com>
2020-01-02 21:11:22 +0300

    update date

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ let scrollVal = 420;
 var app2 = new Vue({
     el: '#updated',
     data: {
-        message: 'Updated: Oct 24th'
+        message: 'Updated: Jan 2nd'
     }
 })
 
 var app3 = new Vue({
     el: '#year',
     data: {
-        year: '2019'
+        year: '2020'
     }
 })
 
